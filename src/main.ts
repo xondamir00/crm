138172b144688d7b54a6b19c174d98544992b6e0
TeamSoftLion <nextnodejs.dev@gmail.com>
2025-12-10 12:15:21 +0500

    url

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ async function bootstrap() {
   app.use(cookieParser());
   app.useLogger(logger);
   app.enableCors({
-    origin: 'https://team-soft-ware-fk6s.vercel.app', // front URL
+    origin: ['http://localhost:5173', 'https://team-soft-ware-fk6s.vercel.app'],
     credentials: true, //cookie uchun majburiy
   });
 
